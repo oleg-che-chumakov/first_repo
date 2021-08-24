89625ce93dae7fa30540aa3d0d5999cbc6fbc127
Чумаков Олег Германович <ChumakovOG@okb-du.local>
2021-08-24 17:25:25 +0300

    Initial commit

---
+++ src/test1/AnotherClass.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class AnotherClass {
+	public static String getName() {
+		return "Name";
+	}
+	
+	static int getAge() {
+		return 10;
+	}
+
+}
---
+++ src/test1/Hello.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Hello {
+	public static void main (String[] args) {
+		System.out.println("Hello, World!");
+		
+		String name = AnotherClass.getName();
+		System.out.println("Name = "+name);
+		
+		int age = AnotherClass.getAge();
+		System.out.println("Age = "+age);
+
+}
+}

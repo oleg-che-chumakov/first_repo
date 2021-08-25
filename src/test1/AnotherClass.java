32cb8f486567e22cf60f16e5d90e93113674cece
Чумаков Олег Германович <ChumakovOG@okb-du.local>
2021-08-25 08:25:26 +0300

    first pull reqest

--- src/test1/AnotherClass.java
+++ src/test1/AnotherClass.java
@@ -2,11 +2,11 @@ package test1;
 
 public class AnotherClass {
 	public static String getName() {
-		return "Name";
+		return "Oleg";
 	}
 	
 	static int getAge() {
-		return 10;
+		return 20;
 	}
 
 }
--- src/test1/Hello.java
+++ src/test1/Hello.java
@@ -2,7 +2,7 @@ package test1;
 
 public class Hello {
 	public static void main (String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello world!");
 		
 		String name = AnotherClass.getName();
 		System.out.println("Name = "+name);

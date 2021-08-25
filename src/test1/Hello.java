a4e0f36827dbec29fa068e4a7785eb47b51c70f8
Чумаков Олег Германович <ChumakovOG@okb-du.local>
2021-08-25 08:32:12 +0300

    hw-001

--- src/test1/Hello.java
+++ src/test1/Hello.java
@@ -2,7 +2,7 @@ package test1;
 
 public class Hello {
 	public static void main (String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello, World!");
 		
 		String name = AnotherClass.getName();
 		System.out.println("Name = "+name);
